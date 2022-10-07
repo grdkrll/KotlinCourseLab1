a5ffdd2f3e3650666334df6f233753bce2578794
grdkrll <grdkrll@gmail.com>
2022-10-07 18:13:57 +0300

    added binary-search tree

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -27,6 +27,55 @@ fun <T : Comparable<T>> MutableList<T>.selectionSort(): MutableList<T> {
     return this
 }
 
+class Node(value: Int) {
+    var v: Int
+    var l: Int?
+    var r: Int?
+
+    init {
+        v = value
+        l = null
+        r = null
+    }
+}
+
+class Tree {
+    private var tree = mutableListOf<Node>()
+
+    fun add(e: Int) {
+        if (tree.isEmpty()) {
+            tree.add(Node(e))
+            return
+        }
+        var i = 0
+        while (tree[i].v != e && (tree[i].v > e && tree[i].l != null || tree[i].v < e && tree[i].r != null)) {
+            i = when (tree[i].v > e) {
+                true -> tree[i].l!!
+                false -> tree[i].r!!
+            }
+        }
+        if (tree[i].v > e) {
+            tree[i].l = tree.size
+        } else if (tree[i].v < e) {
+            tree[i].r = tree.size
+        } else {
+            return
+        }
+        tree.add(Node(e))
+    }
+
+    fun withTwoChildren(): List<Int> {
+        val r = mutableListOf<Int>()
+        for (e in tree) {
+            if (e.l != null && e.r != null) {
+                r.add(e.v)
+            }
+        }
+        r.selectionSort()
+        return r.toList()
+    }
+}
+
 fun main() {
     val simpleList = mutableListOf(2, 13, 14, 1, 6, 16, 18, 19, 3, 8)
     simpleList.forEach { print("$it ") }
@@ -37,4 +86,11 @@ fun main() {
     simpleList.forEach { print("$it ") }
     println()
     simpleList.selectionSort().forEach { print("$it ") }
+    println()
+    val arr: List<Int> = readln().split(' ').map { it.toInt() }
+    val tree = Tree()
+    for (e in arr) {
+        tree.add(e)
+    }
+    tree.withTwoChildren().forEach { println(it) }
 }
